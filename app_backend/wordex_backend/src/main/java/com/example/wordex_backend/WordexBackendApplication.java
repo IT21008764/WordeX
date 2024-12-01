03e1314c4726c28413ff3056e740d51cce4c99bb
Namal Jayawardana <namaljayawardana2001@gmail.com>
2024-12-01 16:24:00 +0530

    namal

--- app_backend/wordex_backend/src/main/java/com/example/wordex_backend/WordexBackendApplication.java
+++ app_backend/wordex_backend/src/main/java/com/example/wordex_backend/WordexBackendApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WordexBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WordexBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(WordexBackendApplication.class, args);
+    }
 }
--- app_backend/wordex_backend/src/main/java/com/example/wordex_backend/model/User.java
+++ app_backend/wordex_backend/src/main/java/com/example/wordex_backend/model/User.java
@@ -1,20 +1,20 @@
 package com.example.wordex_backend.model;
 
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.annotation.Id;
 
-@Document(collection = "users") // Maps to the "users" collection in MongoDB
+import lombok.Getter;
+import lombok.Setter;
+
+@Document(collection = "users")
+@Getter
+@Setter
 public class User {
+
     @Id
-    private String id;
+    private String id;  // Use String for MongoDB ID
+    
+    private String name;
     private String email;
     private String password;
-
-    // Getters and Setters
-    public String getId() { return id; }
-    public void setId(String id) { this.id = id; }
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
 }
